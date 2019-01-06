8ca07088e4b26d894edcb010aaffa57fb1acc632
Rhome Falaize <rhome.falaize@gmail.com>
2019-01-05 20:29:20 -0500

    Multi GPUs

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -19,7 +19,7 @@ model.summary()
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data() 
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 train_images = train_images.reshape((60000, 28, 28, 1))
 train_images = train_images.astype('float32') / 255
@@ -34,8 +34,8 @@ test_labels = to_categorical(test_labels)
 from keras import backend
 backend.tensorflow_backend._get_available_gpus()
 
-model.compile(optimizer='rmsprop', 
-              loss='categorical_crossentropy', 
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(train_images, train_labels, epochs=5, batch_size=64)
@@ -43,6 +43,7 @@ model.fit(train_images, train_labels, epochs=5, batch_size=64)
 # test
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('test_acc=', test_acc, '; test_loss=', test_loss)
+
 
 
 # Multi GPUs
